81964c24b320008c0c5e5654cc02053f657a1313
Amanda Pouget <mandysimon88@gmail.com>
2018-06-01 12:34:18 -0400

    Add a passing and failing test for Deck as a starting place

---
+++ app/deck.js
@@ -0,0 +1,11 @@
+class Deck {
+  cardsLeft() {
+    return 52;
+  }
+
+  deal() {
+    
+  }
+}
+
+module.exports = Deck;
--- spec/deckSpec.js
+++ spec/deckSpec.js
@@ -1,5 +1,15 @@
-// const Deck = require('../app/deck');
-//
-// describe('Deck', () => {
-//
-// });
+const Deck = require('../app/deck');
+
+describe('Deck', () => {
+  it('has 52 cards when created', () => {
+    const deck = new Deck();
+    expect(deck.cardsLeft()).toEqual(52);
+  });
+
+  it('deals the top card', () => {
+    const deck = new Deck();
+    const card = deck.deal();
+    expect(card).toBeDefined();
+    expect(deck.cardsLeft()).toEqual(51);
+  });
+});
